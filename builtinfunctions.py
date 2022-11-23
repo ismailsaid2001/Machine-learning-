4620e2b16ae9c68c0a44de1006c488f712ff4c41
Ismail <ismail.said@insat.u-carthage.tn>
2022-11-23 01:12:23 +0100

    5

--- builtinfunctions.py
+++ builtinfunctions.py
@@ -14,3 +14,12 @@ print(f.read())
 f.close()
 with open('fichier.txt','r') as f :
     print(f.read())
+#ex1
+f=open("fichier.txt",'w')
+for i in range(10):
+    f.write(f"{i}^2 ={i*2}\n")
+f.close()
+with open("fichier.txt",'r') as f :
+    L=[]
+    L=(f.read()).split("\n")
+    print(L)
